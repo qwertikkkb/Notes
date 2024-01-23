27c541e30e050270eabb4747946a8621638b8827
qwertikkkb <147443772+qwertikkkb@users.noreply.github.com>
2024-01-23 20:21:15 +0200

    First Commit

---
+++ Notes/Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Nicolai Bodean on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
